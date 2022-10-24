57a9fbdf041022b224dc5730b32d4258bf83bd6f
Santiago <gomezgu31@gmail.com>
2022-10-24 07:37:06 -0600

    04-gif-expert-app

---
+++ 03-counter-app-vite/src/base-pruebas/05-funciones.js
@@ -0,0 +1,11 @@
+
+export const getUser = () => ({
+        uid: 'ABC123',
+        username: 'Gsdesign_502'
+});
+
+
+export const getUsuarioActivo = ( nombre ) =>({
+    uid: 'ABC567',
+    username: nombre
+})
