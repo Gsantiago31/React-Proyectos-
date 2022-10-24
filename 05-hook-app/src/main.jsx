1b771e2f66aa213a3811c0306c4255cb025ffba9
Santiago <gomezgu31@gmail.com>
2022-10-24 15:28:20 -0600

    section 10

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { TodoApp } from './08-useReducer/TodoApp'
 
 
 // import { HooksApp } from './HooksApp'
@@ -13,13 +14,14 @@ import ReactDOM from 'react-dom/client'
 // import { Memorize } from './06-memos/Memorize'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { CallBackHook } from './06-memos/CallBackHook'
-import { Padre } from './07-tarea-memo/Padre'
+// import { Padre } from './07-tarea-memo/Padre'
+// import './08-useReducer/intro-reducer'
 
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Padre/>
-  </React.StrictMode>
+  // <React.StrictMode>
+    <TodoApp/>
+  // </React.StrictMode>
 )
