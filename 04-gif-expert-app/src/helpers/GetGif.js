2df5425eb08a1f1ebb035b67c04711834095ade0
Santiago <gomezgu31@gmail.com>
2022-11-05 21:28:54 -0600

    journalApp deploy

--- 04-gif-expert-app/src/helpers/GetGif.js
+++ 04-gif-expert-app/src/helpers/GetGif.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
 
-    const url = `api.giphy.com/v1/gifs/search?api_key=tAZZSXELJdcQYfVrIGX5AdC2zZUxp20W=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=tAZZSXELJdcQYfVrIGX5AdC2zZUxp20W&q=${category}&limit=20`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
